b0ad2df5d82dbbe72ce62d56d674362fc2fd780a
dlyahov <dy.lyahov@gmail.com>
2016-08-24 23:27:10 +0300

    Added web pack.

---
+++ README.md
@@ -0,0 +1,3 @@
+#Panzer
+
+##The best game
---
+++ render/render.js
@@ -0,0 +1,43 @@
+var ctx, canvas, images;
+canvas = document.getElementById('canvas');
+ctx = canvas.getContext('2d');
+
+function drawMap(fieldOfComponents) {
+    for (let i = 0; i < fieldOfComponents.length; i++) {
+        for (let j = 0; j < fieldOfComponents[i].length; j++) {
+            ctx.drawImage(fieldOfComponents[i][j], fieldOfComponents[i][j].width * i,
+                            fieldOfComponents[i][j].height * j);
+        }
+    }
+}
+
+function myAlert(str) {
+    alert(str);
+}
+
+function drawRect(x, y) {
+    ctx.beginPath();
+    ctx.rect(x, y, 50, 80);
+    ctx.fillStyle = "#f00";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPanzer(userPanzer) {
+    var size = userPanzer.getSize(),
+        centerX = userPanzer.getX() - size.width / 2,
+        centerY = userPanzer.getY() - size.height / 2;
+    ctx.save();
+
+    ctx.translate(centerX, centerY);
+
+    ctx.rotate(userPanzer.getRotation() * Math.PI / 180);
+    ctx.drawImage(userPanzer.getImage(), -(size.width / 2), -(size.height / 2));
+    ctx.restore();
+}
+
+module.exports = {
+    drawRect : drawRect,
+    drawPanzer : drawPanzer,
+    myAlert : myAlert
+}
